39db40f8cda36243814443e62bb41953099a0fa7
Jason Plank <plank.jason@outlook.com>
2023-09-12 05:57:18 +0200

    Using sessions

---
+++ controllers/auth.js
@@ -0,0 +1,15 @@
+exports.getLogin = (req, res, next) => {
+  //const isLoggedIn = req.get('Cookie').split('=')[1];
+  //console.log(req.get('Cookie')); //.split('=')[1];
+  console.log(req.session.isLoggedIn);
+  res.render('auth/login', {
+    pageTitle: 'Login',
+    path: '/login',
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  req.session.isLoggedIn = true;
+  res.redirect('/');
+};
--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -25,6 +26,7 @@ exports.getProduct = (req, res, next) => {
         product: product,
         pageTitle: product.title,
         path: '/products',
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
@@ -37,6 +39,7 @@ exports.getIndex = (req, res, next) => {
         prods: products,
         pageTitle: 'Shop',
         path: '/',
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => {
@@ -53,6 +56,7 @@ exports.getCart = async (req, res, next) => {
         pageTitle: 'Your Cart',
         path: '/cart',
         products: products,
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
@@ -112,6 +116,7 @@ exports.getOrders = (req, res, next) => {
         pageTitle: 'Your Orders',
         path: '/orders',
         orders: orders,
+        isAuthenticated: req.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
