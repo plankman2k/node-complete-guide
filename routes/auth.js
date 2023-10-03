1b6af1f2382f4cb03da0253b84e098693dc3e46e
Jason Plank <plank.jason@outlook.com>
2023-10-03 12:35:55 +0200

    updated validations

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,19 @@ router.get('/login', authController.getLogin);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post(
+  '/login',
+  [
+    body('email').isEmail().withMessage('Please enter a valid email.'),
+    body(
+      'password',
+      'Password should be only number and text and at least 5 characters.'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
+  authController.postLogin
+);
 
 router.post(
   '/signup',
